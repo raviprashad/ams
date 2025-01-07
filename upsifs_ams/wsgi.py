798a1e836dae18c73bd119265585a04368e8a0af
raviprashad <ravisuraj23112006@gmail.com>
2025-01-07 18:51:17 +0530

    Initial commit

---
+++ upsifs_ams/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for upsifs_ams project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'upsifs_ams.settings')
+
+application = get_wsgi_application()
